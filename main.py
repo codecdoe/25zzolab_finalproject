2a7d2ac0156098f964f8598bc99c71a36a7f379a
codecdoe <code0125@hansung-sh.hs.kr>
2025-06-03 18:52:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -120,9 +120,9 @@ if address:
         st.error("❌ 주소를 찾을 수 없습니다. 도로명 주소를 다시 확인해주세요.")
     else:
         temp, weather = get_weather(lat, lon)
-        st.success(f"📍 위치: **{address}**  
+        st.success(f"""📍 위치: **{address}**  
 🌡 현재 기온: **{temp:.1f}°C**  
-🌥 날씨 상태: **{weather}**")
+🌥 날씨 상태: **{weather}**""")
 
         recommendation = get_recommendations(temp, weather)
 
